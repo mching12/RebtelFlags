4abe3fc971a6fe6dd33aef1063169b9759b4bdff
Mark Ching <chingmark12@gmail.com>
2023-04-12 18:30:34 +0800

    Cleanup
    - Update library dependencies
    - Replace deprecated package Parcelize to newer version

--- app/src/main/java/com/android/rebtelflags/data/model/Name.kt
+++ app/src/main/java/com/android/rebtelflags/data/model/Name.kt
@@ -2,7 +2,7 @@ package com.android.rebtelflags.data.model
 
 import android.os.Parcelable
 import com.google.gson.annotations.Expose
-import kotlinx.android.parcel.Parcelize
+import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class Name (
